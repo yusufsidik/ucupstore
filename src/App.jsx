8a2ca65530536c56cdc0702b3ae8c226d4fc2fbc
yusufsidik <yusufsidik36@gmail.com>
2023-08-01 13:07:01 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="h-full min-h-screen min-w-full bg-gradient-to-b from-sky-200 to-orange-300 flex items-center justify-center flex-col">
+      <h1 className="font-bold text-2xl text-slate-800">
+        Coming Soon Ucup Store
+      </h1>
+      <p>Build with React and Tailwind CSS</p>
+    </div>
+  );
+}
+
+export default App;
